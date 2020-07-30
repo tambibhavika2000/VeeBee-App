7d8eb5f9fd21059403cd7e7daed8494c9dec1812
Bhavika2902 <tambibhavika2000@gmail.com>
2020-07-30 12:02:48 +0530

    Modified

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ conn= mysql.connector.connect(host="localhost",user="root",password="bhavikatamb
 cursor=conn.cursor()
 
 @app.route('/')
-def index():
+def home():
     if 'id' in session:
-        return redirect('/option')
+        return redirect('/home')
     else:
-        return render_template('index.html')
+        return render_template('first.html')
 
 @app.route('/register')
 def register():
     return render_template('registers.html')
 
-@app.route('/option')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
 def option():
-    return render_template('option.html')
+    return render_template('home.html')
 
 @app.route('/login_validation', methods=['POST'])
 def valid():
@@ -30,11 +34,26 @@ def valid():
 
     cursor.execute("""SELECT * FROM `users` where `email` like '{}' and `password` like '{}'""".format(email,psw))
     users=cursor.fetchall()
+    session['name']= users[0][1]
+    session['email']= users[0][2]
     if len(users)>0:
         session['id']=users[0][0]
-        return redirect('/option')
+        return redirect('/home')
     else:
         return redirect('/register')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', name=session['name'], email=session['email'])
+
+@app.route('/logout')
+def logout():
+    try:
+        session.pop('id')
+    except:
+        return redirect('/index')
+    return redirect('/index')
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
@@ -43,12 +62,15 @@ def add_user():
     upsw=request.form.get('upsw')
     cpsw=request.form.get('cpsw')
     if upsw==cpsw:
-        cursor.execute("""INSERT INTO `users` (`id`,`name`,`email`,`password`) values
-        (NULL,'{}','{}','{}')""".format(name,email,upsw))
-        conn.commit()
-        return redirect('/')
+        try:
+            cursor.execute("""INSERT INTO `users` (`id`,`name`,`email`,`password`) values
+            (NULL,'{}','{}','{}')""".format(name,email,upsw))
+            conn.commit()
+            return redirect('/index')
+        except:
+            return ("User already exist . Login with your credentials")
     else:
-        return redirect('/register')
+        return "password does not match"
 
 
 if __name__=="__main__":
